dbbf52dd081aa18825a368f5a46df6926a1d6909
Comum <miguel.rib.20@gmail.com>
2021-02-25 23:47:24 +0000

    chore: add header and translations

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+const selectElement = document.querySelector("#language");
+
+function load(language) {
+  axios.get("data/info.json").then(function (res) {
+    const translations = res.data[language];
+
+    document.querySelector("#above-header-name").innerHTML =
+      translations.firstname + " " + translations.lastname;
+    document.querySelector("#above-header-label-phone").innerHTML =
+      translations.label_phone;
+    document.querySelector("#above-header-phone").innerHTML =
+      translations.phone;
+    document.querySelector("#above-header-label-email").innerHTML =
+      translations.label_email;
+    document.querySelector("#above-header-email").innerHTML =
+      translations.email;
+  });
+}
+
+selectElement.addEventListener("change", function () {
+  load(this.value);
+});
