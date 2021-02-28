8dc3fd38a6170d50f5c68a5914540429d165b04d
Comum <miguel.rib.20@gmail.com>
2021-02-28 15:16:22 +0000

    chore: add print to pdf funcionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
 const selectElement = document.querySelector("#language");
+let pageTitle = "";
+let isReadyToPrint = false;
 
 function load(language) {
   axios.get("data/info.json").then(function (res) {
     const translations = res.data[language];
+
+    // set name and print variable
+    pageTitle = `${translations.firstname} ${translations.lastname} CV`;
+    isReadyToPrint = true;
 
     // above header
     document.querySelector("#above-header-name").innerHTML =
@@ -22,26 +28,50 @@ function load(language) {
     document.querySelector("#header-title").innerHTML = translations.title;
 
     // intro section
-    document.querySelector("#intro-section-text").innerHTML = translations.intro;
-    document.querySelector("#intro-section-details-phone").innerHTML = translations.label_phone;
-    document.querySelector("#intro-section-details-phone-value").innerHTML = translations.phone;
-    document.querySelector("#intro-section-details-email").innerHTML = translations.label_email;
-    document.querySelector("#intro-section-details-email-value").innerHTML = translations.email;
-    document.querySelector("#intro-section-details-linkedin").innerHTML = translations.label_linkedin;
-    document.querySelector("#intro-section-details-linkedin-value").innerHTML = translations.linkedin_handle;
-    document.querySelector("#intro-section-details-linkedin-value").href = translations.linkedin_url;
-    document.querySelector("#intro-section-details-github").innerHTML = translations.label_github;
-    document.querySelector("#intro-section-details-github-value").innerHTML = translations.github_handle;
-    document.querySelector("#intro-section-details-github-value").href = translations.github_url;
+    document.querySelector("#intro-section-text").innerHTML =
+      translations.intro;
+    document.querySelector("#intro-section-details-phone").innerHTML =
+      translations.label_phone;
+    document.querySelector("#intro-section-details-phone-value").innerHTML =
+      translations.phone;
+    document.querySelector("#intro-section-details-email").innerHTML =
+      translations.label_email;
+    document.querySelector("#intro-section-details-email-value").innerHTML =
+      translations.email;
+    document.querySelector("#intro-section-details-linkedin").innerHTML =
+      translations.label_linkedin;
+    document.querySelector("#intro-section-details-linkedin-value").innerHTML =
+      translations.linkedin_handle;
+    document.querySelector("#intro-section-details-linkedin-value").href =
+      translations.linkedin_url;
+    document.querySelector("#intro-section-details-github").innerHTML =
+      translations.label_github;
+    document.querySelector("#intro-section-details-github-value").innerHTML =
+      translations.github_handle;
+    document.querySelector("#intro-section-details-github-value").href =
+      translations.github_url;
 
     // section headers
-    document.querySelector("#section-header-skills").innerHTML = translations.label_skills;
-    document.querySelector("#section-header-history").innerHTML = translations.label_history;
-    document.querySelector("#section-header-education").innerHTML = translations.label_education;
-    document.querySelector("#section-header-additional").innerHTML = translations.label_additional;
+    document.querySelector("#section-header-skills").innerHTML =
+      translations.label_skills;
+    document.querySelector("#section-header-history").innerHTML =
+      translations.label_history;
+    document.querySelector("#section-header-education").innerHTML =
+      translations.label_education;
+    document.querySelector("#section-header-additional").innerHTML =
+      translations.label_additional;
   });
 }
 
 selectElement.addEventListener("change", function () {
   load(this.value);
+});
+
+document.querySelector("#print-button").addEventListener("click", function () {
+  if (!isReadyToPrint) {
+    alert("Please pick a language");
+    return;
+  }
+
+  window.print();
 });
