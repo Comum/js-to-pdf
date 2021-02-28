34894f2f5439e873bd1d13f5838570937841b2be
Comum <miguel.rib.20@gmail.com>
2021-02-28 12:45:41 +0000

    chore: add intro part

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ function load(language) {
   axios.get("data/info.json").then(function (res) {
     const translations = res.data[language];
 
+    // above header
     document.querySelector("#above-header-name").innerHTML =
       translations.firstname + " " + translations.lastname;
     document.querySelector("#above-header-label-phone").innerHTML =
@@ -15,9 +16,23 @@ function load(language) {
     document.querySelector("#above-header-email").innerHTML =
       translations.email;
 
+    // header
     document.querySelector("#header-name").innerHTML =
       translations.firstname + " " + translations.lastname;
     document.querySelector("#header-title").innerHTML = translations.title;
+
+    // intro section
+    document.querySelector("#intro-section-text").innerHTML = translations.intro;
+    document.querySelector("#intro-section-details-phone").innerHTML = translations.label_phone;
+    document.querySelector("#intro-section-details-phone-value").innerHTML = translations.phone;
+    document.querySelector("#intro-section-details-email").innerHTML = translations.label_email;
+    document.querySelector("#intro-section-details-email-value").innerHTML = translations.email;
+    document.querySelector("#intro-section-details-linkedin").innerHTML = translations.label_linkedin;
+    document.querySelector("#intro-section-details-linkedin-value").innerHTML = translations.linkedin_handle;
+    document.querySelector("#intro-section-details-linkedin-value").href = translations.linkedin_url;
+    document.querySelector("#intro-section-details-github").innerHTML = translations.label_github;
+    document.querySelector("#intro-section-details-github-value").innerHTML = translations.github_handle;
+    document.querySelector("#intro-section-details-github-value").href = translations.github_url;
   });
 }
 
